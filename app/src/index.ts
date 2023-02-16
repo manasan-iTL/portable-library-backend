eedd14fb3e472c4925ccacaff5bbfa05ae8d5121
manasan-iTL <daiyaofssh2020@gmail.com>
2023-02-17 02:55:49 +0900

    ユーザーのRead機能の作成

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,12 +1,27 @@
 
+import { PrismaClient } from "@prisma/client";
 import express, { Request, Response } from "express";
+
+const prisma = new PrismaClient()
 
 const app = express()
 const PORT = 3000;
 
+/*  Expressのミドルウェアの設定  */
+app.use(express.json())
+
 app.get("/", (req: Request, res: Response) => {
     res.send("Wlecome to Pocket Library!")
 })
+
+app.get("/users", async (req: Request, res: Response) => {
+    const allUsers = await prisma.user.findMany()
+    res.json({ allUsers })
+})
+
+// app.post("/users", async (req: Request, res: Response) => {
+
+// })
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
