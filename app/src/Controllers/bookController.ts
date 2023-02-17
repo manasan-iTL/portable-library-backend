7ff097781d8bd3511c98666fa057edf25150e182
manasan-iTL <daiyaofssh2020@gmail.com>
2023-02-17 18:52:18 +0900

    Bookの一覧を返すAPI作成

--- app/src/Controllers/bookController.ts
+++ app/src/Controllers/bookController.ts
@@ -1,5 +1,12 @@
 import Router, { Request, Response } from 'express'
+import prisma from '../orm/prismaClien'
+import { TypedRequestBody, TypedRequestQuery } from '../interfaces/common'
+import { createBook } from '../interfaces/bookType'
 
 const router = Router()
 
+router.get("/", async (req: Request, res: Response) => {
+    const books = await prisma.book.findMany()
+    res.json({ books })
+})
 export default router
