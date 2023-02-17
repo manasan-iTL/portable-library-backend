484ce892ab838886c13b491fabea5f67340048cf
manasan-iTL <daiyaofssh2020@gmail.com>
2023-02-17 18:51:46 +0900

    bookControllerの定義とインポート

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import userRouter from "./Controllers/userController";
+import bookRouter from "./Controllers/bookController"
 
 const app = express()
 const PORT = 3000;
@@ -7,6 +8,7 @@ const PORT = 3000;
 /*  Expressのミドルウェアの設定  */
 app.use(express.json())
 app.use("/users", userRouter)
+app.use("/books", bookRouter)
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Wlecome to Pocket Library!")
