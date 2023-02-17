ce8b7109e9929f7bc3a00bba8a3a1d78af733535
manasan-iTL <daiyaofssh2020@gmail.com>
2023-02-17 16:47:53 +0900

    User作成、読み取りAPI作成 それに関わる型定義のインポート

--- app/src/Controllers/userController.ts
+++ app/src/Controllers/userController.ts
@@ -1,10 +1,30 @@
 import { Request, Response, Router } from "express";
+import { TypedRequestBody, TypedRequestQuery } from "../interfaces/common";
+import { CreateUser } from "../interfaces/userType";
 import prisma from "../orm/prismaClien";
+
 const router = Router()
 
 router.get("/", async (req: Request, res: Response) => {
     const allUsers = await prisma.user.findMany()
     res.json({ allUsers })
+})
+
+router.post("/", async (req: TypedRequestBody<CreateUser>, res: Response) => {
+    const { email, password, username } = req.body
+    const user = await prisma.user.create({
+        data : { email, password, username }
+    })
+
+    res.json({ user })
+})
+
+router.get("/:user_id", async (req:Request<{ user_id: string }>, res: Response ) => {
+    const userId = req.params?.user_id
+    const user = await prisma.user.findUnique({
+        where: { id: parseInt(userId) },
+    });
+    res.json({ user });
 })
 
 export default router
