4c098ff54f45c654c3db7e9a5a83d64f44266b4d
manasan-iTL <daiyaofssh2020@gmail.com>
2023-02-19 21:21:49 +0900

    貸し出しを行うAPIの作成（まだ途中）

--- app/src/Controllers/rentalController.ts
+++ app/src/Controllers/rentalController.ts
@@ -39,4 +39,48 @@ router.get("/users/:user_id", async (req: Request, res: Response) => {
     res.json({ rentaledBooks })
 })
 
+router.post("/",async (req:Request, res: Response) => {
+
+    const { book_id, user_id } = req.body
+
+    try {
+        
+            // 本が借りることができるか試す→エラーを返す
+            const rentableBook = await prisma.rentable_Book.findUnique({
+                where: { id: parseInt(book_id) }
+            })
+
+            if (rentableBook === null) {
+                throw new Error("借りることができません。");
+            }
+
+            // 該当の本を削除する
+            await prisma.rentable_Book.delete({
+                where: { id: parseInt(book_id) }
+            })
+
+            // 貸し出しテーブルに登録
+            // const rentaledBook = await prisma.user.update({
+            //     where: { id: user_id },
+            //     data: {
+            //         rentaled_books: {
+                        
+            //         }
+            //     }
+            // })
+
+    } catch (error) {
+        
+    }
+
+    const rentaledBook = await prisma.user.update({
+        where: { id: parseInt(user_id)},
+        data: {
+            rentaled_books: {
+
+            }
+        }
+    })
+})
+
 export default router
