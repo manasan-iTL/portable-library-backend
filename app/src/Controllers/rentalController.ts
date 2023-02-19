a17e09d83e717bb4b8023a2abf83df54364e8d84
manasan-iTL <daiyaofssh2020@gmail.com>
2023-02-19 21:21:05 +0900

    特定のユーザーが借りている本の一覧を返すAPi作成

--- app/src/Controllers/rentalController.ts
+++ app/src/Controllers/rentalController.ts
@@ -14,5 +14,29 @@ router.get("/all", async (req: Request, res: Response) => {
     res.json({ rentaledBooks })
 })
 
+router.get("/users/:user_id", async (req: Request, res: Response) => {
+    const userId = req.params?.user_id
+    const rentaledBooks = await prisma.user.findUnique({
+        where: {
+            id: parseInt(userId)
+        },
+        select: {
+            rentaled_books: {
+                select: {
+                    book: {
+                        select: {
+                            id: true,
+                            title: true,
+                            author: true,
+                            book_type: true
+                        }
+                    }
+                }
+            }
+        }
+    })
+
+    res.json({ rentaledBooks })
+})
 
 export default router
